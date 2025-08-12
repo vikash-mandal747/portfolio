aa53de5396adcf7196b5b0360729d744a3bfebe6
vikash-mandal747 <vikashmandal747@gmail.com>
2025-08-12 19:08:38 +0530

    protfolio

---
+++ index.js
@@ -0,0 +1,126 @@
+// ----------------------------------button to take you top starts -------------------------------------//
+var mybutton = document.getElementById("myBtn");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+ function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// ---------------------------------------button to take you top end --------------------------------------//
+
+ 
+
+
+
+
+var string = "Vikash Kumar Mandal";
+var str = string.split("");
+var el = document.getElementById("user-detail-name");
+
+(function animate() {
+  str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
+  var running = setTimeout(animate, 90);
+})();
+
+ 
+
+ const text = document.querySelector(".typing-dev");
+
+// make a words array
+const words = [
+ 
+  "Full Stack Developer..."
+];
+
+// start typing effect
+setTyper(text, words);
+
+function setTyper(element, words) {
+  const LETTER_TYPE_DELAY = 75;
+  const WORD_STAY_DELAY = 2000;
+
+  const DIRECTION_FORWARDS = 0;
+  const DIRECTION_BACKWARDS = 1;
+
+  var direction = DIRECTION_FORWARDS;
+  var wordIndex = 0;
+  var letterIndex = 0;
+
+  var wordTypeInterval;
+
+  startTyping();
+
+  function startTyping() {
+    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+  }
+
+  function typeLetter() {
+    const word = words[wordIndex];
+
+    if (direction == DIRECTION_FORWARDS) {
+      letterIndex++;
+
+      if (letterIndex == word.length) {
+        direction = DIRECTION_BACKWARDS;
+        clearInterval(wordTypeInterval);
+        setTimeout(startTyping, WORD_STAY_DELAY);
+      }
+    } else if (direction == DIRECTION_BACKWARDS) {
+      letterIndex--;
+
+      if (letterIndex == 0) {
+        nextWord();
+      }
+    }
+
+    const textToType = word.substring(0, letterIndex);
+
+    element.textContent = textToType;
+  }
+
+  function nextWord() {
+    letterIndex = 0;
+    direction = DIRECTION_FORWARDS;
+    wordIndex++;
+
+    if (wordIndex == words.length) {
+      wordIndex = 0;
+    }
+  }
+}
+
+
+ 
+
+
+
+
+ 
+
+
+//Resume button line
+document.getElementById('resume-link-2').addEventListener("click",()=>{
+  window.open("https://drive.google.com/file/d/1p0SwJ2Weq4QRKemKwB1OhlFLw3q3-SXa/view?usp=sharing","_blank")
+})
+
+ 
+
+ 
+
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "vikash-mandal747", { responsive: true });
+ 
